38f76d9bb7ee263206cbc8ec748d3f4a6854f4d7
aleix-canet <aleix.canet@zartis.com>
2025-09-03 16:39:47 +0200

    chore: add basename to router

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -1,9 +1,14 @@
-import { createBrowserRouter } from "react-router";
-import SavingsCalculator from "../pages/SavingsCalculator";
+import { createBrowserRouter } from 'react-router';
+import SavingsCalculator from '../pages/SavingsCalculator';
 
-export const router = createBrowserRouter([
+export const router = createBrowserRouter(
+  [
+    {
+      path: '/',
+      element: <SavingsCalculator />,
+    },
+  ],
   {
-    path: '/',
-    element: <SavingsCalculator />,
+    basename: '/lumo-saving-calculator/',
   },
-]);
+);
